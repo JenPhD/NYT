8300c16802fd180a66e57a14829c049a7e53c975
JenPhD <jpemberton76@gmail.com>
2016-08-01 20:32:47 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,12 @@
+//Create an html file called NYTSearch.html, put everything inside
+//it like you are going to create a bootstrap site and create a linked javascript 
+//file (after the linked jQuery that works with Bootstrap 2.2.4?).  
+//Test the javascript file with a console.log ("Made it"); sanity check.  
+//You may want to keep this 'blank template' around and it will definitely help 
+//out in class tomorrow.  The class pace will feel much more comfortable if you can 
+//get at least some time spent on this beforehand.
+
+$(document).ready(function () {
+	console.log("Made it");
+
+});//ending document ready.
